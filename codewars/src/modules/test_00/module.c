8dd4e056967e30911449f407fb9e993ce5222b45
Sebastián Del Moral <sebmorgal@gmail.com>
2023-10-07 12:15:53 +0200

    Feat: Add test env for codewars

---
+++ codewars/src/modules/test_00/module.c
@@ -0,0 +1,19 @@
+/**
+ * @file test_mod.c
+ * @author Sebastián Del Moral
+ * @brief Test module source.
+ * @version 0.1
+ * @date 2023-09-30
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <stdio.h>
+
+void module_run(void)
+{
+    printf("Module 00\n");
+}
+
+/* EOF */
